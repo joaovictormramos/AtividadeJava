cf4865e08ee6ae2b10d8714eee04953c1d19cf67
joaovictormramos <molinajoao10@gmail.com>
2024-01-29 18:19:12 -0400

    Classe posição

---
+++ Estudos/Sistema-xadrez-Java/src/application/Application.java
@@ -0,0 +1,12 @@
+package application;
+
+import tabuleiro.Posicao;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Posicao posicao = new Posicao(1,3);
+		System.out.print(posicao.toString());
+	}
+
+}
